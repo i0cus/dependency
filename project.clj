d51e32147c850e8c7111d5daec14cbb86a22e97c
James Reeves <jreeves@weavejester.com>
2018-01-21 21:42:41 +0000

    Change project group name for fork

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.stuartsierra/dependency "0.2.1-SNAPSHOT"
+(defproject weavejester/dependency "0.2.1-SNAPSHOT"
   :description "A data structure for representing dependency graphs"
-  :url "https://github.com/stuartsierra/dependency"
+  :url "https://github.com/weavejester/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
